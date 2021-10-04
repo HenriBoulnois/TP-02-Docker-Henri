43c1f0536e9694c09fd435a308e45237167182a6
HenriBoulnois <72384047+HenriBoulnois@users.noreply.github.com>
2021-10-04 10:04:03 +0200

    Update commands.sh

--- 0_run/commands.sh
+++ 0_run/commands.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+docker container run docker/whalesay cowsay "Hello M1 III 2021"
+docker container run --rm docker/whalesay cowsay "Hello M1 III 2021"
+docker container run -it python
+docker container run --rm -it python
